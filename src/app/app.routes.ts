1c6a4b210cc13f2f105b5a3ee9c551d78c9bd404
Viktoriia Yemelianova <v.yemelianova@mobidev.biz>
2025-08-28 23:04:14 +0300

    setup lazy routing & add envs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'contact-us',
+    loadComponent: () => import('./pages/contact-us/contact-us.component').then((m) => m.ContactUsComponent),
+  },
+  {
+    path: 'horoscope/:type',
+    loadChildren: () => import('./pages/horoscope/horoscope.routes').then((m) => m.horoscopeRoutes),
+  },
+  {
+    path: '404',
+    loadComponent: () => import('./pages/not-found/not-found.component').then((m) => m.NotFoundComponent),
+  },
+  { path: '**', redirectTo: '404', pathMatch: 'full' },
+];
---
+++ src/app/pages/horoscope/sign-details/sign-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-details',
+  imports: [],
+  templateUrl: './sign-details.component.html',
+  styleUrl: './sign-details.component.scss'
+})
+export class SignDetailsComponent {
+
+}
