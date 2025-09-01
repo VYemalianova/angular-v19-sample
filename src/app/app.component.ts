22e96676aa34a6a71e7a5bb1bf107364b29bcea2
Viktoriia Yemelianova <v.yemelianova@mobidev.biz>
2025-09-01 16:31:32 +0300

    add vertical menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,36 @@
-import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { Component, DestroyRef, inject, OnInit, viewChild } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 
 import { HeaderComponent } from './components/header/header.component';
 import { BreakpointService } from './services/breakpoint/breakpoint.service';
 import { HorizontalMenuComponent } from './components/nav-menu/horizontal-menu/horizontal-menu.component';
+import { VerticalMenuComponent } from './components/nav-menu/vertical-menu/vertical-menu.component';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSidenavModule, HeaderComponent, HorizontalMenuComponent],
+  imports: [RouterOutlet, MatSidenavModule, HeaderComponent, HorizontalMenuComponent, VerticalMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private router = inject(Router);
+  private destroyRef = inject(DestroyRef);
   breakpointService = inject(BreakpointService);
+
+  private sidenav = viewChild.required<MatSidenav>(MatSidenav);
+
+    ngOnInit(): void {
+    this.router.events.pipe(
+      takeUntilDestroyed(this.destroyRef),
+      filter(e => e instanceof NavigationEnd))
+        .subscribe(() => {
+          if (this.sidenav().opened) {
+            this.sidenav().close();
+          }
+        });
+  }
+
 }
