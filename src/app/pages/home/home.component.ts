36100c5b54f68a6c7217d2eb6a1dc1ebef97b7ff
Viktoriia Yemelianova <v.yemelianova@mobidev.biz>
2025-09-26 13:35:27 +0300

    add about us section on home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect, ElementRef, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  private aboutUsSection = viewChild<ElementRef<HTMLDivElement>>('aboutUsSection');
 
+  constructor() {
+    effect(() => {
+      const content = this.aboutUsSection();
+      const scrollTo = history.state.scrollTo;
+
+      if (content && scrollTo === 'about-us') {
+        content.nativeElement.scrollIntoView({ behavior: 'smooth' });
+        history.replaceState({}, '');
+      }
+    });
+  }
 }
